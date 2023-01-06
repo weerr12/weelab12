3946c0a6398beb4126089f60e5b859672ec93d58
werasak mayer <jokerwwe0542@gmail.com>
2023-01-06 14:53:02 +0700

    "lab12-1 complete"

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -5,22 +5,22 @@
 using namespace std;
 
 int main(){
-    srand(time(0));
-    cout << "Press Enter 3 times to reveal you future.";
+    cout << "Press Enter 3 times to reveal your future.";
     for (int i=0;i<3;i++){
         cin.get();
     }
+    srand(time(0));
     int x =rand()%9;
     string grade;
     if(x==0) grade="A";
-    else if(x==2) grade="B+";
-    else if(x==3) grade="B";
-    else if(x==4) grade="C+";
-    else if(x==5) grade="C";
-    else if(x==6) grade="D+";
-    else if(x==7) grade="D";
-    else if(x==8) grade="F";
-    else if(x==9) grade="W";
+    else if(x==1) grade="B+";
+    else if(x==2) grade="B";
+    else if(x==3) grade="C+";
+    else if(x==4) grade="C";
+    else if(x==5) grade="D+";
+    else if(x==6) grade="D";
+    else if(x==7) grade="F";
+    else if(x==8) grade="W";
     
     cout << "You will get "<< grade << " in this 261102.";
 }
